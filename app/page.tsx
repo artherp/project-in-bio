b7b3d6c9dc204b716b7ac5e9dc8fc006e993bb4f
artherp <arthurfonsecaerpen@gmail.com>
2025-01-30 22:04:02 -0300

    start of hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Hero from "./components/landing-page/hero";
 
 export default function Home() {
   return (
-    <div className="bg-background-primary">
-      Hello World
+    <div className="max-w-7xl mx-auto border"> 
+    {/* limitamos o tamanho da tela para 7xl e centralizamos, para n se preocupar com telas muito grander */}
+      
+      <Hero />
+
     </div>
   );
 }
