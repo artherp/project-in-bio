5ee81dca9450ce6eb8972878dc086f63bc1118bb
artherp <arthurfonsecaerpen@gmail.com>
2025-01-31 18:34:47 -0300

    create link page done

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import Hero from "./components/landing-page/hero";
-import Header from "./components/landing-page/header";
-import VideoExplanation from "./components/landing-page/video-explanation";
-import Pricing from "./components/landing-page/pricing";
-import FAQ from "./components/landing-page/faq";
+import Hero from "@/app/components/landing-page/hero";
+import Header from "@/app/components/landing-page/header";
+import VideoExplanation from "@/app/components/landing-page/video-explanation";
+import Pricing from "@/app/components/landing-page/pricing";
+import FAQ from "@/app/components/landing-page/faq";
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto "> 
