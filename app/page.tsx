9be62a55417dfe5a6e39b2f66dd4803bb11e3927
artherp <arthurfonsecaerpen@gmail.com>
2025-01-31 16:04:36 -0300

    creating video and pricing sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import Hero from "./components/landing-page/hero";
 import Header from "./components/landing-page/header";
+import VideoExplanation from "./components/landing-page/video-explanation";
+import Pricing from "./components/landing-page/pricing";
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto "> 
     {/* limitamos o tamanho da tela para 7xl e centralizamos, para n se preocupar com telas muito grander */}
       <Header />
       <Hero />
-
+      <VideoExplanation />
+      <Pricing />
     </div>
   );
 }
