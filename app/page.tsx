090e38b5b686f2ebf993b49880e0b003490d45a7
artherp <arthurfonsecaerpen@gmail.com>
2025-01-31 15:26:42 -0300

    creating the header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import Hero from "./components/landing-page/hero";
-
+import Header from "./components/landing-page/header";
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto "> 
     {/* limitamos o tamanho da tela para 7xl e centralizamos, para n se preocupar com telas muito grander */}
-      
+      <Header />
       <Hero />
 
     </div>
